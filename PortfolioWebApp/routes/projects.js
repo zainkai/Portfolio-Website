4299726537f6bc67186056793edd11316687ad9d
Kevin Turkington <turkingtonkevin@gmail.com>
2016-08-16 23:09:42 -0700

    added project view templating functionality and test data

--- PortfolioWebApp/routes/projects.js
+++ PortfolioWebApp/routes/projects.js
@@ -1,9 +1,37 @@
 ﻿var express = require('express');
 var router = express.Router();
 
+var testData = {
+    data: [
+        {
+            Name: 'Project Name test',
+            subHeading:'Subheading test',
+            imgLink: ['./images/projects/test/test1.png', './images/projects/test/test2.png', './images/projects/test/test3.png'],
+            Description: 'lorem ispsum',
+            githubLink:'http://www.google.com'
+        },
+        {
+            Name: 'Project Name test 22222222222222222',
+            subHeading: 'Subheading test',
+            imgLink: ['./images/projects/test/test1.png', './images/projects/test/test2.png', './images/projects/test/test3.png'],
+            Description: 'lorem ispsum',
+            githubLink: 'http://www.google.com'
+        },
+        {
+            Name: 'Project Name test 3333333',
+            subHeading: 'Subheading test',
+            imgLink: ['./images/projects/test/test1.png', './images/projects/test/test2.png', './images/projects/test/test3.png'],
+            Description: 'lorem ispsum',
+            githubLink: 'http://www.google.com'
+        }
+    ]
+};
+
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('Projects');
+    res.render('Projects', testData);
 });
+
+
 
 module.exports = router;
