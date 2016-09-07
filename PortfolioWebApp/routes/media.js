d51eaa77e4b3710efc4a3afb6aa86b336970cd19
Kevin Turkington <turkingtonkevin@gmail.com>
2016-09-06 23:19:54 -0700

    added 3 projects

--- PortfolioWebApp/routes/media.js
+++ PortfolioWebApp/routes/media.js
@@ -1,9 +1,24 @@
 ﻿var express = require('express');
 var router = express.Router();
 
+var mediaData = {
+    data: [
+        {
+            Link: "https://github.com/zainkai",
+            img: "images/media_images/github.png",
+            description: "Github"
+        },
+        {
+            Link: "https://www.linkedin.com/in/kevin-turkington-b211a798",
+            img: "images/media_images/linkedin.png",
+            description: "Linkedin"
+        }
+    ]
+}
+
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('Media');
+    res.render('Media',mediaData);
 });
 
 module.exports = router;
--- PortfolioWebApp/routes/projects.js
+++ PortfolioWebApp/routes/projects.js
@@ -4,25 +4,25 @@ var router = express.Router();
 var testData = {
     data: [
         {
-            Name: 'Project Name test',
-            subHeading:'Subheading test',
-            imgLink: ['./images/projects/test/test1.png', './images/projects/test/test2.png', './images/projects/test/test3.png'],
-            Description: 'lorem ispsum description 1',
-            githubLink:'http://www.google.com'
+            Name: 'Portfolio Website',
+            subHeading:'Personal Project',
+            imgLink: ['./images/projects/portfolioWebsite/portfolioProjects.PNG', './images/projects/portfolioWebsite/Capture1.png', './images/projects/portfolioWebsite/Capture2.png'],
+            Description: 'This is a Basic website to display my skills with nodejs and development of a website',
+            githubLink:'https://github.com/zainkai/Portfolio-Website'
         },
         {
-            Name: 'Project Name test 22222222222222222',
-            subHeading: 'Subheading test',
-            imgLink: ['./images/projects/test/test1.png', './images/projects/test/test2.png', './images/projects/test/test3.png'],
-            Description: 'lorem ispsum',
-            githubLink: 'http://www.google.com'
+            Name: 'i3-wm',
+            subHeading: 'Desktop Manager theme',
+            imgLink: ['./images/projects/i3/logo.png', './images/projects/i3/Capture1.png', './images/projects/i3/Capture2.png'],
+            Description: 'A custom i3 windown manager theme',
+            githubLink: 'https://github.com/zainkai/i3-wm'
         },
         {
-            Name: 'Project Name test 3333333',
-            subHeading: 'Subheading test',
-            imgLink: ['./images/projects/test/test1.png', './images/projects/test/test2.png', './images/projects/test/test3.png'],
-            Description: 'lorem ispsum',
-            githubLink: 'http://www.google.com'
+            Name: 'ASME-Website (In Development)',
+            subHeading: 'Collaboration Project',
+            imgLink: ['./images/projects/asme/asme.png', './images/projects/asme/shell1.jpeg', './images/projects/asme/osu.png'],
+            Description: 'Website for Oregon state Universitys ASME Shell eco marathon club. Developed by Jonah Spencer and myself.',
+            githubLink: 'https://github.com/jonah1217/asme-website'
         }
     ]
 };
